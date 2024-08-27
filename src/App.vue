3e4d2f1b75bbffec4fbc0f17dd079cefc76556e2
Rodrigo Menna <rmenna.barroso@gmail.com>
2024-08-26 20:52:57 -0400

    projeto base

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <h1>Projeto Produtos</h1>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
+
+<style>
+</style>

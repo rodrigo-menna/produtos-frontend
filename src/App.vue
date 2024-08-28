24dc6d6a07deb91ea73a4bfa58d153c4c4fdbbc2
Rodrigo Menna <rmenna.barroso@gmail.com>
2024-08-28 18:30:24 -0400

    criando o componente BarraLateral e configurando o app.vue para renderizar as pages

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,47 @@
 <template>
-  <main>
-    <h1>Projeto Produtos</h1>
+  <main
+    class="columns is-gapless is-multiline"
+    :class="{ 'modo-escuro': modoEscuro }"
+  >
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarters conteudo">
+      <router-view></router-view>
+    </div>
   </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
+import BarraLateral from "./components/BarraLateral.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
+  components: {
+    BarraLateral,
+  },
+  data() {
+    return {
+      modoEscuro: true,
+    };
+  },
 });
 </script>
 
 <style>
+main {
+  --bg-primario: #fff;
+  --texto-primario: #000;
+}
+main.modo-escuro {
+  --bg-primario: #2b2d42;
+  --texto-primario: #ddd;
+}
+.lista {
+  padding: 1.25rem;
+}
+.conteudo {
+  background-color: var(--bg-primario);
+}
 </style>
---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,71 @@
+<template>
+  <header>
+    <h1>
+      <img alt="Gerenciador de Produtos" src="../assets/product-manager.png" width="100rem">
+    </h1>
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <router-link to="/" class="link">
+            <i class="fas fa-house"></i>
+            Home
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/produtos" class="link">
+            <i class="fas fa-tasks"></i>
+            Produtos
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/categorias" class="link">
+            <i class="fas fa-project-diagram"></i>
+            Categoria
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BarraLateral",
+  data() {
+    return {};
+  },
+});
+</script>
+<style scoped>
+h1 {
+  text-align: center;
+}
+strong {
+  color: #f95738;
+}
+header {
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+  padding: 2rem;
+}
+@media only screen and (max-width: 768px) {
+  header {
+    height: auto;
+  }
+}
+.panel li {
+  margin: 8px 0;
+}
+.link {
+  color: #fff;
+}
+.link:hover {
+  color: #faf0ca;
+}
+.link.router-link-active {
+  color: #faf0ca;
+}
+</style>

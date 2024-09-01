8fefe2a6ad05fe45804da1dcb67f3dfe617fa0c3
Rodrigo Menna <rmenna.barroso@gmail.com>
2024-09-01 08:00:46 -0400

    instalando e configurando axios

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import '@fortawesome/fontawesome-free/css/all.css'
+import "@fortawesome/fontawesome-free/css/all.css";
+import axios from "./plugins/axios";
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .use(axios, {
+    baseUrl: "http://localhost:8080/",
+  })
+  .mount("#app");
